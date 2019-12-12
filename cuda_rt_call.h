c9470cc4ee19f72d877d0821ecd08fff44d190f7
Bei Wang <beiwang@princeton.edu>
2019-12-12 12:01:39 -0500

    add abort()

--- cuda_rt_call.h
+++ cuda_rt_call.h
@@ -7,11 +7,14 @@
     {                                                                                       \
         cudaError_t cudaStatus = call;                                                      \
         if (cudaSuccess != cudaStatus)                                                      \
+        {                                                                                   \
             fprintf(stderr,                                                                 \
                     "ERROR: CUDA RT call \"%s\" in line %d of file %s failed "              \
                     "with "                                                                 \
                     "%s (%d).\n",                                                           \
                     #call, __LINE__, __FILE__, cudaGetErrorString(cudaStatus), cudaStatus); \
+            abort();                                                                        \
+        }                                                                                   \
     }
 
 #endif
